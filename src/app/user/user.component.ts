ef0132fb5908e6ddc624aea1e7a42023f1b62912
WardaArshad <90782818+WardaArshad@users.noreply.github.com>
2024-09-03 22:05:07 +0500

    User object uing type / interfaces

--- src/app/user/user.component.ts
+++ src/app/user/user.component.ts
@@ -1,6 +1,18 @@
 import { Component, computed, EventEmitter, input, Input, Output } from '@angular/core';
 import { DUMMY_USERS } from '../dummy-users';
 
+// type User = {
+//   id: string;
+//   avatar: string;
+//   name: string;
+// }
+
+interface User {
+  id: string;
+  avatar: string;
+  name: string;
+}
+
 @Component({
   selector: 'app-user',
   standalone: true,
@@ -9,11 +21,7 @@ import { DUMMY_USERS } from '../dummy-users';
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  @Input({ required: true }) user! : {
-    id: string;
-    avatar: string;
-    name: string;
-  }
+  @Input({ required: true }) user! : User;
   @Output() select = new EventEmitter();
 
   ImagePath(){
