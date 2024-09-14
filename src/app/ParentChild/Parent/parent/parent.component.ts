f518a9e4588717e914e4eb08147cc821b8d89fad
WardaArshad <90782818+WardaArshad@users.noreply.github.com>
2024-09-15 01:13:20 +0500

    Parent Child Communication (Input Output)

---
+++ src/app/ParentChild/Parent/parent/parent.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+import { Patient } from '../../patient.model';
+import { ChildComponent } from '../../child/child/child.component';
+
+@Component({
+  selector: 'app-parent',
+  standalone: true,
+  imports: [ChildComponent],
+  templateUrl: './parent.component.html',
+  styleUrl: './parent.component.css'
+})
+export class ParentComponent {
+  selectedPatient?: Patient;
+  patients: Patient[] = [
+    {id: '01', name: 'Ali', age: 20},
+    {id: '02', name: 'Ayesha', age: 25},
+    {id: '03', name: 'Noor', age: 30}
+  ]
+
+  AddPatient(){
+    this.patients.push({id: this.patients.length.toString(), name: 'Wow', age: 35 + this.patients.length});
+  }
+
+  handleChildResponse(id: string){
+    this.selectedPatient = this.patients.find((patient) => patient.id === id);
+  }
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,13 +4,15 @@ import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
 import { DUMMY_USERS } from './dummy-users';
 import { TasksComponent } from './tasks/tasks.component';
+import { ParentComponent } from "./ParentChild/Parent/parent/parent.component";
+import { ChildComponent } from "./ParentChild/child/child/child.component";
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent]
+    imports: [HeaderComponent, UserComponent, TasksComponent, ParentComponent, ChildComponent]
 })
 export class AppComponent {
   users = DUMMY_USERS;
