7a03f50f63d0a7ebec2989597559222a26bc381a
WardaArshad <90782818+WardaArshad@users.noreply.github.com>
2024-09-16 23:47:45 +0500

    Routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 
 import { HeaderComponent } from './header/header.component';
 import { UserComponent } from './user/user.component';
@@ -8,17 +8,24 @@ import { PatientsComponent } from "./PatientDetails/patients/patients.component"
 import { DetailsComponent } from './PatientDetails/PatientDetails/details.component';
 import { ChildComponent } from './ParentChild/child/child.component';
 import { ParentComponent } from './ParentChild/Parent/parent.component';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from '@angular/router';
 
 @Component({
     selector: 'app-root',
     standalone: true,
     templateUrl: './app.component.html',
     styleUrl: './app.component.css',
-    imports: [HeaderComponent, UserComponent, TasksComponent, ParentComponent, ChildComponent, PatientsComponent, DetailsComponent]
+    imports: [
+      HeaderComponent, UserComponent, TasksComponent, //User and its Tasks
+      ParentComponent, ChildComponent, //Parent Child
+      PatientsComponent, DetailsComponent, //Parent Child
+      RouterOutlet, RouterLink, RouterLinkActive //Routing
+    ]
 })
 export class AppComponent {
   users = DUMMY_USERS;
   selectedUserId?: string;
+  router = inject(Router);
 
   get selectedUser() {
     return this.users.find((user) => user.id === this.selectedUserId)!;
@@ -26,5 +33,10 @@ export class AppComponent {
 
   onSelectUser(id: string) {
     this.selectedUserId = id;
+  }
+
+  Routing(){
+    // this.router.navigateByUrl("/app-second");
+    this.router.navigate(["app-second"]);
   }
 }
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
 import { Routes } from '@angular/router';
+import { FirstComponent } from './Routing/first/first.component';
+import { SecondComponent } from './Routing/second/second.component';
+import { RouterlinkComponent } from './Routing/routerlink/routerlink.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: 'app-first', component: FirstComponent},
+    {path: 'app-second', component: SecondComponent},
+    {path: 'app-routerlink', component: RouterlinkComponent}
+];
