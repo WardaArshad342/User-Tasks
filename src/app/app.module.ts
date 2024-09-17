fee3fbcf0e624f4878146f1e4779be1b4d39f98d
WardaArshad <90782818+WardaArshad@users.noreply.github.com>
2024-09-17 22:45:35 +0500

    Restructuring of imports

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
+import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
+import { CommonModule, NgStyle } from "@angular/common";
+import { FormsModule } from "@angular/forms";
+
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
 import { TasksComponent } from "./tasks/tasks.component";
-import { ParentComponent } from "./ParentChild/Parent/parent.component";
-import { DetailsComponent } from "./PatientDetails/PatientDetails/details.component";
-import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
-import { CardComponent } from "./shared/card/card.component";
-import { ChildComponent } from "./ParentChild/child/child.component";
-import { PatientsComponent } from "./PatientDetails/patients/patients.component";
-import { CommonModule, NgStyle } from "@angular/common";
-import { FormsModule } from "@angular/forms";
 import { TaskComponent } from "./tasks/task/task.component";
 import { NewTaskComponent } from "./tasks/new-task/new-task.component";
+import { CardComponent } from "./shared/card/card.component";
+
+import { ParentComponent } from "./ParentChild/Parent/parent.component";
+import { DetailsComponent } from "./PatientDetails/PatientDetails/details.component";
+import { ChildComponent } from "./ParentChild/child/child.component";
+import { PatientsComponent } from "./PatientDetails/patients/patients.component";
 
 @NgModule({
     declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, NewTaskComponent,//User and its Tasks
