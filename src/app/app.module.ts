b9636061bf040d27924481ef47c6ec34e0187267
WardaArshad <90782818+WardaArshad@users.noreply.github.com>
2024-09-17 23:05:06 +0500

    Multiple Modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,29 +2,25 @@ import { NgModule } from "@angular/core";
 import { BrowserModule } from '@angular/platform-browser';
 import { Router, RouterLink, RouterLinkActive, RouterOutlet } from "@angular/router";
 import { CommonModule, NgStyle } from "@angular/common";
-import { FormsModule } from "@angular/forms";
 
 import { AppComponent } from "./app.component";
 import { HeaderComponent } from "./header/header.component";
 import { UserComponent } from "./user/user.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { TaskComponent } from "./tasks/task/task.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
-import { CardComponent } from "./shared/card/card.component";
 
 import { ParentComponent } from "./ParentChild/Parent/parent.component";
 import { DetailsComponent } from "./PatientDetails/PatientDetails/details.component";
 import { ChildComponent } from "./ParentChild/child/child.component";
 import { PatientsComponent } from "./PatientDetails/patients/patients.component";
+import { SharedModule } from "./shared/shared.module";
+import { TasksModule } from "./tasks/tasks.module";
 
 @NgModule({
-    declarations: [AppComponent, HeaderComponent, UserComponent, TasksComponent, TaskComponent, NewTaskComponent,//User and its Tasks
+    declarations: [AppComponent, HeaderComponent, UserComponent,
         ParentComponent, ChildComponent, //Parent Child
         DetailsComponent, PatientsComponent, //Parent Child
-        CardComponent
         ],
     bootstrap: [AppComponent],
-    imports: [BrowserModule, CommonModule, NgStyle, FormsModule,
+    imports: [BrowserModule, CommonModule, NgStyle, SharedModule, TasksModule,
         RouterOutlet, RouterLink, RouterLinkActive //Routing
     ]
 })
